25f8acd5e4f353beeb73c9d96d54613866a530fc
rajkunwar4 <40rajkunwar@gmail.com>
2025-04-14 13:10:26 +0530

    cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
  // Lib middlewares
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://finzarc-assignment.vercel.app/'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
